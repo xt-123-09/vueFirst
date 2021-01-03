7cf751b3de90c14e557b74561ba46288996a06bd
xt <1290031552@qq.com>
2021-01-03 22:26:42 +0800

    'first'

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
+
+    <el-row>
+  <el-button round>圆角按钮</el-button>
+  <el-button type="primary" round>主要按钮</el-button>
+  <el-button type="success" round>成功按钮</el-button>
+  <el-button type="info" round>信息按钮</el-button>
+  <el-button type="warning" round>警告按钮</el-button>
+  <el-button type="danger" round>危险按钮</el-button>
+</el-row>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
-</template>
+</template> 
 
 <style>
 #app {
@@ -28,5 +37,7 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+
+  
 }
 </style>
